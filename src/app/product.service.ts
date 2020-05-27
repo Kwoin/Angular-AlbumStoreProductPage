a07683d4386312d66dba097020ab0c6931fdffa7
pwilleme <pierric.willemet@capgemini.com>
2020-05-27 17:44:34 +0200

    fix outdated import to add...

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _http: Http, private httpClient: HttpClient, ) { }
+  constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<any> {
-    return this.httpClient.get(this._albumUrl);
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json);
   }
 
 }
