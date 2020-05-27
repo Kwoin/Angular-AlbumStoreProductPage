1e8082e5dca588a87c32a83d8be9edd6a90909c5
pwilleme <pierric.willemet@capgemini.com>
2020-05-27 17:39:35 +0200

    fix outdated import to add...

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
