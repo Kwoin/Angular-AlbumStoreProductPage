6c214d2552217f0e2ad76b575a0d0620e6791517
pwilleme <pierric.willemet@capgemini.com>
2020-05-27 17:33:35 +0200

    step3

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ProductService {
+
+  private albumUrl = '../assets/album.json';
+
+  constructor(private http: HttpClient) { }
+
+  getAlbum(id: number): Observable<any> {
+    return this.http.get(this.albumUrl);
+  }
+
+}
