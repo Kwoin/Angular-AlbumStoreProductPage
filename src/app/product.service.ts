75a9deadcd8003f7c695e69252a906bcc24125e9
pwilleme <pierric.willemet@capgemini.com>
2020-05-27 17:53:45 +0200

    step4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -11,8 +12,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json);
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album> response.json());
   }
 
 }
